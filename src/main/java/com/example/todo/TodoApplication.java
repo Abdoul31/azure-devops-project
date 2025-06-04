4da85ac4b934b1d4cd9c4da3ab07a65366af8f5a
Abdoul31 <daradjikine@gmail.com>
2025-06-04 19:36:18 +0000

    Test CI ajout d'un log de demarrage avec logback

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -1,11 +1,20 @@
 package com.example.todo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoApplication {
+       
+    private static final Logger logger = LoggerFactory.getLogger(TodoApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
+
+        logger.info("Application démarre avec Logback Test CI GitHub Actions");
     }
-}
+    }
+
+
